f692cec23182ece70d75a8784a56edb94160b98f
TronkaD <103572992+TronkaD@users.noreply.github.com>
2024-08-22 11:01:09 +0000

    Update forecast.js
    gestion des erreurs

--- js/forecast.js
+++ js/forecast.js
@@ -1,11 +1,21 @@
 const apiKey = '8e96cc80225e961a033630880bcca97d';
 
 export async function getForecast(city) {
-    const reponse = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`);
-    if(!reponse.ok){
-        throw new Error('Echec lors de la récupération des données');
+    try{
+        
+        const reponse = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`);
+        
+        if(!reponse.ok){
+            throw new Error('Echec lors de la récupération des données');
+        }
+        
+        const data = await reponse.json();
+        return data;
+        
+    } catch (error) {
+        console.log('Erreur : ', error.message);
+        return null;
     }
-    const data = await reponse.json();
-    return data;
+    
 }
 
